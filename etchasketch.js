1993d06e2d57309fbf6317d8c352535cf08a5a97
Kelly <126833824+kraftwerkit@users.noreply.github.com>
2024-04-03 21:12:17 -0700

    Styling, Generalized Grid, Generate Trail

--- etchasketch.js
+++ etchasketch.js
@@ -1,16 +1,33 @@
 const SketchArea = document.querySelector("#sketch-area");     //Define sketch area
+const SketchAreaSide=600 //The width/height from CSS as a numerical value (no dimensions)
 
-//Create a 16x16 grid //
 
-function createGrid() {
-  for (let i=1; i<=256; i++) {                     //This loop creates 256 divs.
+//Generates a rainbow pixelated trail
+function pixel() {
+  this.style.backgroundColor=rainbow(); //Why does "this" work?
+}
+
+//Generates grid size with user input
+function createGrid(side) {
+  for (let i=1; i<=side**2; i++) {
     const box = document.createElement("div")
-    box.style.width=box.style.height="37.5px"  /// 600/16=37.5
+    box.style.width=box.style.height=`${SketchAreaSide/side}px`
     box.classList.add("box")
-    SketchArea.appendChild(box);
+    SketchArea.appendChild(box); //Adds the divisions to the container
+    box.addEventListener("mouseover",pixel) //Why does the event listener need to be here?
 }
 }
 
-createGrid()
+createGrid(16)
 
-//Create a black pixelated trail //
+
+//Create an array for colors used in trail
+const pretty = ["#ffadad",  "#ffd6a5",  "#fdffb6",  "#caffbf",  
+"#9bf6ff",  "#a0c4ff",  "#bdb2ff","#ffc6ff"]
+
+//Returns a random color value from the array
+function rainbow() { 
+  {
+       return pretty[Math.floor(Math.random()*pretty.length)];
+   }
+  }
